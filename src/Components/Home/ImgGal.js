baba13df9c58b8ff122293ae97489058d887c2ad
janna <rijanuy88@gmail.com>
2021-07-28 13:57:17 +0800

    ADDED: Image Gallery

--- src/Components/Home/ImgGal.js
+++ src/Components/Home/ImgGal.js
@@ -1,15 +1,153 @@
+import sample2014_01 from '../../Assets/Photos/2014/1.jpeg'
+import sample2014_02 from '../../Assets/Photos/2014/2.jpeg'
+import sample2014_03 from '../../Assets/Photos/2014/3.jpeg'
+import sample2014_04 from '../../Assets/Photos/2014/4.jpeg'
+import sample2014_05 from '../../Assets/Photos/2014/5.jpeg'
+import sample2014_06 from '../../Assets/Photos/2014/6.jpeg'
+
+import sample2015_01 from '../../Assets/Photos/2015/1.jpeg'
+import sample2015_02 from '../../Assets/Photos/2015/2.jpeg'
+import sample2015_03 from '../../Assets/Photos/2015/3.jpeg'
+import sample2015_04 from '../../Assets/Photos/2015/4.jpeg'
+
+import sample2016_01 from '../../Assets/Photos/2016/1.jpeg'
+import sample2016_02 from '../../Assets/Photos/2016/2.jpeg'
+import sample2016_03 from '../../Assets/Photos/2016/3.jpeg'
+import sample2016_04 from '../../Assets/Photos/2016/4.jpeg'
+
+import sample2017_01 from '../../Assets/Photos/2017/1.jpeg'
+import sample2017_02 from '../../Assets/Photos/2017/2.jpeg'
+import sample2017_03 from '../../Assets/Photos/2017/3.jpeg'
+
+import sample2018_01 from '../../Assets/Photos/2018/1.jpeg'
+import sample2018_02 from '../../Assets/Photos/2018/2.jpeg'
+import sample2018_03 from '../../Assets/Photos/2018/3.jpeg'
+import sample2018_04 from '../../Assets/Photos/2018/4.jpeg'
+import sample2018_05 from '../../Assets/Photos/2018/5.jpeg'
+
+import sample2019_01 from '../../Assets/Photos/2019/1.jpeg'
+import sample2019_02 from '../../Assets/Photos/2019/2.jpeg'
+import sample2019_03 from '../../Assets/Photos/2019/3.jpeg'
+
+import sample2020_01 from '../../Assets/Photos/2020/1.jpeg'
+import sample2020_02 from '../../Assets/Photos/2020/2.jpeg'
+
 // image gallery
-const images = [
+export const ImgGal = [
     {
-        original: src="D:\AAAACADS_1STSEM\SYSDEV\indayog\src\Assets\Photos\02014\53767_713943711987657_6952282712969468280_o.jpeg",
-        thumbnail: 'https://picsum.photos/id/1018/250/150/',
+        // original: 'https://picsum.photos/id/1018/1000/600/',
+        // original: `src: ${sample}`,
+        original: `${sample2014_01}`,
+        thumbnail: `${sample2014_01}`,
     },
     {
-        original: 'https://picsum.photos/id/1015/1000/600/',
-        thumbnail: 'https://picsum.photos/id/1015/250/150/',
+        original: `${sample2014_02}`,
+        thumbnail: `${sample2014_02}`,
     },
     {
-        original: 'https://picsum.photos/id/1019/1000/600/',
-        thumbnail: 'https://picsum.photos/id/1019/250/150/',
+        original: `${sample2014_03}`,
+        thumbnail: `${sample2014_03}`,
+    },
+    {
+        original: `${sample2014_04}`,
+        thumbnail: `${sample2014_04}`,
+    },
+    {
+        original: `${sample2014_05}`,
+        thumbnail: `${sample2014_05}`,
+    },
+    {
+        original: `${sample2014_06}`,
+        thumbnail: `${sample2014_06}`,
+    },
+    // 2015
+    {
+        original: `${sample2015_01}`,
+        thumbnail: `${sample2015_01}`,
+    },
+    {
+        original: `${sample2015_02}`,
+        thumbnail: `${sample2015_02}`,
+    },
+    {
+        original: `${sample2015_03}`,
+        thumbnail: `${sample2015_03}`,
+    },
+    {
+        original: `${sample2015_04}`,
+        thumbnail: `${sample2015_04}`,
+    },
+    //2016
+    {
+        original: `${sample2016_01}`,
+        thumbnail: `${sample2016_01}`,
+    },
+    {
+        original: `${sample2016_02}`,
+        thumbnail: `${sample2016_02}`,
+    },
+    {
+        original: `${sample2016_03}`,
+        thumbnail: `${sample2016_03}`,
+    },
+    {
+        original: `${sample2016_04}`,
+        thumbnail: `${sample2016_04}`,
+    },
+    // 2017
+    {
+        original: `${sample2017_01}`,
+        thumbnail: `${sample2017_01}`,
+    },
+    {
+        original: `${sample2017_02}`,
+        thumbnail: `${sample2017_02}`,
+    },
+    {
+        original: `${sample2017_03}`,
+        thumbnail: `${sample2017_03}`,
+    },
+    // 2018
+    {
+        original: `${sample2018_01}`,
+        thumbnail: `${sample2018_01}`,
+    },
+    {
+        original: `${sample2018_02}`,
+        thumbnail: `${sample2018_02}`,
+    },
+    {
+        original: `${sample2018_03}`,
+        thumbnail: `${sample2018_03}`,
+    },
+    {
+        original: `${sample2018_04}`,
+        thumbnail: `${sample2018_04}`,
+    },
+    {
+        original: `${sample2018_05}`,
+        thumbnail: `${sample2018_05}`,
+    },
+    // 2019
+    {
+        original: `${sample2019_01}`,
+        thumbnail: `${sample2019_01}`,
+    },
+    {
+        original: `${sample2019_02}`,
+        thumbnail: `${sample2019_02}`,
+    },
+    {
+        original: `${sample2019_03}`,
+        thumbnail: `${sample2019_03}`,
+    },
+    // 2020
+    {
+        original: `${sample2020_01}`,
+        thumbnail: `${sample2020_01}`,
+    },
+    {
+        original: `${sample2020_02}`,
+        thumbnail: `${sample2020_02}`,
     },
 ];
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,33 +5,35 @@ import Sidebar from '../Components/Home/Sidebar'
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
 import Charts from '../Components/Home/Charts';
-import sample from '../Assets/Photos/0o32014/one.jpeg'
+import { ImgGal } from '../Components/Home/ImgGal';
+
 
 function Home() {
     // image gallery
-    const images = [
-        {
-            // original: 'https://picsum.photos/id/1018/1000/600/',
-            // original: `src: ${sample}`,
-            original: `${sample}`,
-            thumbnail: 'https://picsum.photos/id/1018/250/150/',
-        },
-        {
-            original: 'https://picsum.photos/id/1015/1000/600/',
-            thumbnail: 'https://picsum.photos/id/1015/250/150/',
-        },
-        {
-            original: 'https://picsum.photos/id/1019/1000/600/',
-            thumbnail: 'https://picsum.photos/id/1019/250/150/',
-        },
-    ];
+    // const images = [
+    //     {
+    //         // original: 'https://picsum.photos/id/1018/1000/600/',
+    //         // original: `src: ${sample}`,
+    //         original: `${sample}`,
+    //         thumbnail: 'https://picsum.photos/id/1018/250/150/',
+    //     },
+    //     {
+    //         original: 'https://picsum.photos/id/1015/1000/600/',
+    //         thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    //     },
+    //     {
+    //         original: 'https://picsum.photos/id/1019/1000/600/',
+    //         thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    //     },
+    // ];
+
     return (
         <div className='home'>
             <Sidebar />
             <div className="home_body">
                 <div className="home_gallery">
                     <h2>Fiesta Through the Years</h2>
-                    <ImageGallery items={images} />
+                    <ImageGallery items={ImgGal} />
                 </div>
                 <div className="home_charts">
                     <h2>Indayog 2021 Cluster Standing</h2>
