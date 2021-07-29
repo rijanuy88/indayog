cf43d41d48684b978a0bb16ef7da6b0e39ef85eb
janna <rijanuy88@gmail.com>
2021-07-29 11:15:09 +0800

    ADDED: clusters data

---
+++ src/Components/Clusters/ClustersData.js
@@ -0,0 +1,47 @@
+export const clustersData = [
+    {
+        img: "https://venngage-wordpress-gallery.s3.amazonaws.com/uploads/2018/10/25.jpg",
+        alt:"ACC",
+        title:'Accountancy',
+    },
+    {
+        img:"https://picsum.photos/id/1018/1000/600/",
+        alt:"BM", 
+        title :'Business & Management',
+    },
+    {
+        img:"https://picsum.photos/id/1018/1000/600/", 
+        alt:"CS",
+        title :'Computer Studies',
+    },
+    {
+        img:"https://picsum.photos/id/1018/1000/600/",
+        alt:"HumLet",
+        title :'Humanities & Letters',
+    },
+    {
+        img:"https://picsum.photos/id/1018/1000/600/",
+        alt:"NSM", 
+        title :'Natural Sciences & Mathematics',
+    },
+    {
+        img:"https://picsum.photos/id/1018/1000/600/",
+        alt:"SEA", 
+        title :'School of Engineering & Architecture',
+    },
+    {
+        img:"https://picsum.photos/id/1018/1000/600/",
+        alt:"SOE",
+        title :'School of Education',
+    },
+    {
+        img:"https://picsum.photos/id/1018/1000/600/",
+        alt:"SON", 
+        title :'School of Nursing',
+    },
+    {
+        img:"https://picsum.photos/id/1018/1000/600/", 
+        alt:"SS", 
+        title :'Social Sciences',
+    },
+]
--- src/Pages/Clusters.js
+++ src/Pages/Clusters.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Cards from '../Components/Clusters/Cards'
+import { clustersData } from '../Components/Clusters/ClustersData'
+
 import './Clusters.css'
 
 function Clusters() {
@@ -7,15 +9,12 @@ function Clusters() {
     return (
         <div className='clusters'>
             <div className="clusters_images">
-                <Cards img="https://venngage-wordpress-gallery.s3.amazonaws.com/uploads/2018/10/25.jpg" alt="ACC" title='Accountancy'/>
-                <Cards img="https://picsum.photos/id/1018/1000/600/" alt="BM" title ='Business & Management'/>
-                <Cards img="https://picsum.photos/id/1018/1000/600/" alt="CS" title ='Computer Studies'/>
-                <Cards img="https://picsum.photos/id/1018/1000/600/" alt="HumLet" title ='Humanities & Letters'/>
-                <Cards img="https://picsum.photos/id/1018/1000/600/" alt="NSM" title ='Natural Sciences & Mathematics'/>
-                <Cards img="https://picsum.photos/id/1018/1000/600/" alt="SEA" title ='School of Engineering & Architecture'/>
-                <Cards img="https://picsum.photos/id/1018/1000/600/" alt="SOE" title ='School of Education'/>
-                <Cards img="https://picsum.photos/id/1018/1000/600/" alt="SON" title ='School of Nursing'/>
-                <Cards img="https://picsum.photos/id/1018/1000/600/" alt="SS" title ='Social Sciences'/>
+                {/* <Cards img="https://venngage-wordpress-gallery.s3.amazonaws.com/uploads/2018/10/25.jpg" alt="ACC" title='Accountancy'/> */}
+                {clustersData.map((cluster) => {
+                    return (
+                        <Cards img={cluster.img} alt={cluster.alt} title={cluster.title} />
+                    )
+                })}
             </div>
             
         </div>
