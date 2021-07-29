d478c79f35c9d96c166d5869084aa485d4ee33b9
janna <rijanuy88@gmail.com>
2021-07-29 09:29:14 +0800

    ADDED: tv data

---
+++ src/Components/SamahanTv/tvData.js
@@ -0,0 +1,40 @@
+
+export const tvData = [
+    {
+        img: "https://images.template.net/wp-content/uploads/2016/04/27133811/Youtube-Thumbnail1.jpg?width=256",
+        alt: "Episode 1: Fiesta Szn",
+        episode: "Episode 1: Fiesta Szn",
+        description: "This episode will reintroduce Fiesta to and generally build hype for the upcoming activities ahead. It will also invite Ateneans to join the official Fiesta Launch.",
+    },
+    {
+        img: "https://images.template.net/wp-content/uploads/2016/04/27133811/Youtube-Thumbnail1.jpg?width=256",
+        alt: "Episode 2: Palarong Atenista 2021",
+        episode: "Episode 2: Palarong Atenista 2021",
+        description: "The episode will serve as a guide to the competitive events or e-sports to be played on Palaro 2021 and important details when and where to anticipate the games. A special game will be given for Ateneans to win special collectibles.",
+    },
+    {
+        img: "https://images.template.net/wp-content/uploads/2016/04/27133811/Youtube-Thumbnail1.jpg?width=256",
+        alt: "Episode 3: Fiesta Events Feature",
+        episode: "Episode 3: Fiesta Events Feature",
+        description: "In this episode we will reveal the list of events that Ateneans shall watch out for this year. Also, a special challenge will be given to Ateneans to win a ₱2000 gift certificate.",
+    },
+    {
+        img: "https://images.template.net/wp-content/uploads/2016/04/27133811/Youtube-Thumbnail1.jpg?width=256",
+        alt: "Episode 4: Fiesta Day Overview",
+        episode: "Episode 4: Fiesta Day Overview",
+        description: "These episodes will prime Ateneans about the events they can attend!",
+    },
+    {
+        img: "https://images.template.net/wp-content/uploads/2016/04/27133811/Youtube-Thumbnail1.jpg?width=256",
+        alt: "Episode 5: Fiesta Day Overview",
+        episode: "Episode 5: Fiesta Day Overview",
+        description: "These episodes will prime Ateneans about the events they can attend!",
+    },
+    {
+        img: "https://images.template.net/wp-content/uploads/2016/04/27133811/Youtube-Thumbnail1.jpg?width=256",
+        alt: "Episode 6: Fiesta Day Overview",
+        episode: "Episode 6: Fiesta Day Overview",
+        description: "These episodes will prime Ateneans about the events they can attend!",
+    },
+
+]
--- src/Pages/Tv.js
+++ src/Pages/Tv.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Episode from '../Components/Episode'
+import Episode from '../Components/SamahanTv/Episode'
+import { tvData } from '../Components/SamahanTv/tvData'
 import './Tv.css'
 
 function Tv() {
@@ -14,7 +15,14 @@ function Tv() {
                 <h2>SAMAHAN TV?</h2>
                 <h4>SAMAHAN TV DESCRIPTION</h4>
                 <div className="tv_right_episodes">
-                    <Episode img="https://images.template.net/wp-content/uploads/2016/04/27133811/Youtube-Thumbnail1.jpg?width=584" alt='ep 1' episode ='Episode 1' description='episode 1 description' />
+                    {/* <Episode img="https://images.template.net/wp-content/uploads/2016/04/27133811/Youtube-Thumbnail1.jpg?width=256" alt='ep 1' episode ='Episode 1' description='episode 1 description' /> */}
+
+                    {tvData.map((tv) => {
+                        return (
+                            <Episode img={tv.img} alt={tv.alt} episode={tv.episode} description={tv.description} />
+                        )
+                    })}
+
                 </div>
             </div>
         </div>
