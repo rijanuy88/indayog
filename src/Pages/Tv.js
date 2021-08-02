450180dc488289163b7484dc67dd548172267c58
janna <rijanuy88@gmail.com>
2021-08-03 03:01:37 +0800

    ADDED: background samahan tv right side

--- src/Pages/Tv.js
+++ src/Pages/Tv.js
@@ -5,15 +5,16 @@ import Typography from '@material-ui/core/Typography';
 import { tvData } from '../Components/SamahanTv/tvData'
 import './Tv.css'
 import Grid from "@material-ui/core/Grid";
+import bg from "../Assets/Backgrounds/SAMAHAN TV BG.jpg";
 
 const right ={
-    backgroundImage:"url('../Assets/Backgrounds/SAMAHAN TV BG.jpg')",
+    backgroundImage:`url('../Assets/Backgrounds/SAMAHAN TV BG.jpg')`,
 }
 
 function Tv() {
     return (
         <Grid container direction="row" justifyContent="space-between" alignItems="flex-start">
-            <Grid style={right} container direction="column" justifyContent="center" alignItems="center" xs={6}>
+            <Grid style={{ backgroundImage: `url(${bg})` }} container direction="column" justifyContent="center" alignItems="center" xs={6}>
                 <img src={logo} style={{width:"50%",height:"50%" }} alt="martin Hall" />
                 <Grid>
                     <Typography variant="p">SAMAHAN TV is an initiative for the upcoming 73rd Ateneo Fiesta for the purposes of information dissemination and to spark interest of Atenans to anticipate the biggest event in Ateneo. 
@@ -21,7 +22,7 @@ function Tv() {
                 </Grid>
                 
             </Grid>
-            <Grid xs={6} container direction="column" justifyContent="space-between" alignItems="flex-start">
+            <Grid style={{backgroundColor: '#DB6A96'}} xs={6} container direction="column" justifyContent="space-between" alignItems="flex-start">
                 <Typography variant="h3" style={{color: '#FFFF', fontFamily:'america'}}>Episode List</Typography>
                 {tvData.map((tv) => {
                         return (
